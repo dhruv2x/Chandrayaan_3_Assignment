8fb6a8e3c38389c7aee707139b899c3ef31862c5
Dhruv <chauhandhruv351@gmail.com>
2023-08-30 00:55:08 +0530

    Added test for complex and repeateted movement

--- test/mainCommand.test.js
+++ test/mainCommand.test.js
@@ -25,7 +25,23 @@ test("Complex Movement", () => {
   };
   const expected = {
     coordinates: [0, 1, -1],
-    direction: "W",
+    direction: "N",
+  };
+
+  const output = mainCommand(commands, start);
+  expect(output).toStrictEqual(expected);
+});
+
+test("for same repeateted command", () => {
+  const commands = ["f", "f", "f", "f", "f"];
+
+  const start = {
+    coordinates: [0, 0, 0],
+    direction: "U",
+  };
+  const expected = {
+    coordinates: [0, 0, 5],
+    direction: "U",
   };
 
   const output = mainCommand(commands, start);
@@ -41,7 +57,7 @@ test("Complex Movement", () => {
   };
   const expected = {
     coordinates: [-2, 0, 0],
-    direction: "S",
+    direction: "W",
   };
 
   const output = mainCommand(commands, start);
@@ -57,7 +73,7 @@ test("Complex Movement", () => {
   };
   const expected = {
     coordinates: [0, 0, 0],
-    direction: "E",
+    direction: "S",
   };
 
   const output = mainCommand(commands, start);
@@ -81,38 +97,6 @@ test("Complex Movement", () => {
 });
 
 test("Complex Movement", () => {
-  const commands = ["f", "f", "f", "f", "f"];
-
-  const start = {
-    coordinates: [5, -2, 1],
-    direction: "E",
-  };
-  const expected = {
-    coordinates: [10, -2, 1],
-    direction: "E",
-  };
-
-  const output = mainCommand(commands, start);
-  expect(output).toStrictEqual(expected);
-});
-
-test("Complex Movement", () => {
-  const commands = ["f", "r", "r", "l", "u"];
-
-  const start = {
-    coordinates: [2, -2, 1],
-    direction: "S",
-  };
-  const expected = {
-    coordinates: [2, -3, 1],
-    direction: "U",
-  };
-
-  const output = mainCommand(commands, start);
-  expect(output).toStrictEqual(expected);
-});
-
-test("Complex Movement", () => {
   const commands = ["f", "r", "d"];
 
   const start = {
@@ -122,56 +106,6 @@ test("Complex Movement", () => {
   const expected = {
     coordinates: [0, 1, 0],
     direction: "D",
-  };
-
-  const output = mainCommand(commands, start);
-  expect(output).toStrictEqual(expected);
-});
-
-test("Complex Movement", () => {
-  const commands = ["d", "l", "b", "b"];
-
-  const start = {
-    coordinates: [0, 0, 0],
-    direction: "W",
-  };
-  const expected = {
-    coordinates: [-2, 0, 0],
-    direction: "E",
-  };
-
-  const output = mainCommand(commands, start);
-  expect(output).toStrictEqual(expected);
-});
-
-test("Complex Movement", () => {
-  const commands = ["r", "r", "r"];
-
-  const start = {
-    coordinates: [1, 1, 1],
-    direction: "N",
-  };
-
-  const expected = {
-    coordinates: [1, 1, 1],
-    direction: "W",
-  };
-
-  const output = mainCommand(commands, start);
-  expect(output).toStrictEqual(expected);
-});
-
-test("Complex Movement", () => {
-  const commands = ["u", "u", "f", "l"];
-
-  const start = {
-    coordinates: [0, 0, 0],
-    direction: "U",
-  };
-
-  const expected = {
-    coordinates: [0, -1, 0],
-    direction: "E",
   };
 
   const output = mainCommand(commands, start);
